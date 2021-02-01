8852eb5a9502c2ce0eb1eb72e43d775c8b989cbc
ppotto <ppotto.ch@gmail.com>
2021-02-01 20:15:35 +0900

    외부 정적 리소스 경로 설정 및 전송받은 multipart file 다운로드
    - 정적 리소스 경로 설정 참고 링크 : https://okky.kr/article/543344
    - multipart file 다운로드 참고 링크 : https://erim1005.tistory.com/47

---
+++ release/src/main/java/com/project/release/config/WebConfig.java
@@ -0,0 +1,47 @@
+package com.project.release.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${resources.location}")
+    private String resourcesLocation;
+    @Value("${resources.uri_path:}")
+    private String resourcesUriPath;
+
+//    private final String uploadImagePath;
+//    public WebConfig(@Value("${custom.path.upload-images}") String uploadImagePath){
+//        this.uploadImagePath = uploadImagePath;
+//    }
+//
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler(".html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        //폴더 설정,,
+//        List<String> imageFolders = Arrays.asList("test", "test2");
+//        for (String imageFolder : imageFolders) {
+//            registry.addResourceHandler("/static/img" + imageFolder + "/**")
+//                    .addResourceLocations("file:///" + uploadImagePath + imageFolder + "/")
+//                    .setCachePeriod(3600)
+//                    .resourceChain(true)
+//                    .addResolver(new PathResourceResolver());
+//        }
+//    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(resourcesUriPath + "/**")
+                .addResourceLocations("file://" + resourcesLocation);
+
+    }
+
+}
--- release/src/main/java/com/project/release/controller/AlbumController.java
+++ release/src/main/java/com/project/release/controller/AlbumController.java
@@ -3,14 +3,24 @@ package com.project.release.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AlbumController {
+    @Value("${resources.location}")
+    private String resourcesLocation;
 
     //, @RequestParam("file") MultipartFile[] productImageFiles
     // @RequestParam("file") MultipartFile file,
@@ -32,11 +42,36 @@ public class AlbumController {
       request.getMultiFormList().stream().forEach(photoRequest ->{
 
           System.out.println(photoRequest.getProfileImage().getOriginalFilename());
+          try {
+              saveFiole(photoRequest.getProfileImage(), resourcesLocation);
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
           System.out.println(photoRequest.getPhotoForm().getTitle());
           System.out.println(photoRequest.getPhotoForm().getDescription());
           System.out.println(photoRequest.getPhotoForm().getNum());
 
       });
-
         }
+
+
+    public void saveFiole(MultipartFile file, String directoryPath) throws IOException {
+        // parent directory를 찾는다.
+        Path directory = Paths.get(directoryPath).toAbsolutePath().normalize();
+
+        // directory 해당 경로까지 디렉토리를 모두 만든다.
+        Files.createDirectories(directory);
+
+        // 파일명을 바르게 수정한다.
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        // 파일명에 '..' 문자가 들어 있다면 오류를 발생하고 아니라면 진행(해킹및 오류방지)
+        Assert.state(!fileName.contains(".."), "Name of file cannot contain '..'");
+        // 파일을 저장할 경로를 Path 객체로 받는다.
+        Path targetPath = directory.resolve(fileName).normalize();
+
+        // 파일이 이미 존재하는지 확인하여 존재한다면 오류를 발생하고 없다면 저장한다.
+        Assert.state(!Files.exists(targetPath), fileName + " File alerdy exists.");
+        file.transferTo(targetPath);
+    }
 }
